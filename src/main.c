e40629c28e2cffc08a89ffde52b0422c8db5b50c
TheMickQc <mickael.marcotte2004@gmail.com>
2024-05-13 01:59:30 -0400

    Adding the mlx_init

--- src/main.c
+++ src/main.c
@@ -6,20 +6,18 @@
 /*   By: mmarcott <mmarcott@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 22:17:36 by mmarcott          #+#    #+#             */
-/*   Updated: 2024/05/08 23:13:35 by mmarcott         ###   ########.fr       */
+/*   Updated: 2024/05/13 01:58:18 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
 int	main(void) {
-	mms_set_alloc_fn(ft_calloc);
-	char	*test = mms_alloc(25, sizeof(char));
+	struct mlx	*mlx;
 
-	for (size_t i = 0; i < 25; i++) {
-		test[i] = 'a';
-	}
-	ft_printf("STR: %s\n", test);
-	mms_kill("", false, 0);
+	if (!(mlx = mlx_init(900, 800, "Cub3d", false)))
+		return (puts(mlx_errno), EXIT_FAILURE);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
 	return (0);
 }
