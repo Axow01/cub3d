bbff336bd75c70923f83ea00fae2591dec13bb10
TheMickQc <mmarcott@student.42quebec.com>
2024-05-15 03:52:51 -0400

    fixing small issue in main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mmarcott <mmarcott@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 22:17:36 by mmarcott          #+#    #+#             */
-/*   Updated: 2024/05/13 01:58:18 by mmarcott         ###   ########.fr       */
+/*   Updated: 2024/05/15 03:52:36 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(void) {
 	struct mlx	*mlx;
 
 	if (!(mlx = mlx_init(900, 800, "Cub3d", false)))
-		return (puts(mlx_errno), EXIT_FAILURE);
+		return (puts(mlx_strerror(mlx_errno)), EXIT_FAILURE);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (0);
